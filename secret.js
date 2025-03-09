e5d7663deb43306fef969be1bcea92015495f032
EshitaZjigyasu <149585624+EshitaZjigyasu@users.noreply.github.com>
2025-03-09 12:38:40 +0530

    Update secret.js

--- secret.js
+++ secret.js
@@ -1,17 +1,17 @@
 function setupSecret(correctIndex) {
-    let encodedSecret = "SCQHBB4O"; // Encrypted version of "H4CK3D!" (Base64 + XOR)
-    let key = 42; // XOR key (can be changed)
+    let encodedSecret = "this_is_it";
+    let key = 42;
 
     function decrypt(base64Str, xorKey) {
-        let decoded = atob(base64Str); // Decode Base64
-        return decoded.split("").map(c => String.fromCharCode(c.charCodeAt(0) ^ xorKey)).join(""); // XOR decrypt
+        let decoded = atob(base64Str);
+        return decoded.split("").map(c => String.fromCharCode(c.charCodeAt(0) ^ xorKey)).join("");
     }
 
     window.checkButton = function(buttonIndex) {
         if (buttonIndex === correctIndex) {
-            let secret = decrypt(encodedSecret, key); // Decrypt the secret
+            let secret = decrypt(encodedSecret, key);
             let codeElement = document.getElementById("secretCode");
-            codeElement.innerText = "Secret Code: " + secret;
+            codeElement.innerText = secret;
             codeElement.style.display = "block";
         } else {
             alert("Wrong button! Try again.");
